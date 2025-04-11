46392e350e48bdaf474880d95f761651022eec8f
Filippenko Pavel <filippenko.ps@phystech.edu>
2025-04-11 16:48:29 +0300

    fix resume mode

--- src/run.py
+++ src/run.py
@@ -43,36 +43,34 @@ else:
     last_model_name, last_head_name = list(models.keys())[0], list(classification_heads.keys())[0]
 
 try:
-    # with profiler.profile(
-    # activities=[profiler.ProfilerActivity.CPU, profiler.ProfilerActivity.CUDA],
-    # on_trace_ready=torch.profiler.tensorboard_trace_handler('./logs/profiler'),
-    # ) as prof:
-    for model_name, model_class in dropwhile(lambda item: item[0] < last_model_name, models.items()):
-        for class_head_name, cl_head in dropwhile(lambda item: item[0] < last_head_name, classification_heads.items()):
-            logger.info(f"Start to train model {model_name} with classification head {class_head_name}")
-            
-            print (f"[DEBUG] currant model name {model_name}, current classification head {class_head_name}")
-            # model = model_class(classification_head=cl_head, n_classes=n_classes)
-            # try:
-            #     model_train_result = train_model(model,
-            #                                     model_name,
-            #                                     device,
-            #                                     num_epochs,
-            #                                     learning_rate,
-            #                                     train_loader,
-            #                                     val_loader,
-            #                                     classification_head_name=class_head_name,
-            #                                     class_weights=class_weights
-            #                                     )
-            #     plot_train_proces(num_epochs,
-            #                     model_train_result.train_losses,
-            #                     model_train_result.val_losses,
-            #                     model_train_result.train_accuracies,
-            #                     model_train_result.val_accuracies,
-            #                     model_name,
-            #                     classification_head_name=class_head_name)
-            # except Exception as ex:
-            #     logger.error(f"During training model {model_name} was caught exception {ex}")
-            #     continue
+    with profiler.profile(
+    activities=[profiler.ProfilerActivity.CPU, profiler.ProfilerActivity.CUDA],
+    on_trace_ready=torch.profiler.tensorboard_trace_handler('./logs/profiler'),
+    ) as prof:
+        for model_name, model_class in dropwhile(lambda item: item[0] != last_model_name, models.items()):
+            for class_head_name, cl_head in dropwhile(lambda item: item[0] != last_head_name, classification_heads.items()):
+                logger.info(f"Start to train model {model_name} with classification head {class_head_name}")
+                model = model_class(classification_head=cl_head, n_classes=n_classes)
+                try:
+                    model_train_result = train_model(model,
+                                                    model_name,
+                                                    device,
+                                                    num_epochs,
+                                                    learning_rate,
+                                                    train_loader,
+                                                    val_loader,
+                                                    classification_head_name=class_head_name,
+                                                    class_weights=class_weights
+                                                    )
+                    plot_train_proces(num_epochs,
+                                    model_train_result.train_losses,
+                                    model_train_result.val_losses,
+                                    model_train_result.train_accuracies,
+                                    model_train_result.val_accuracies,
+                                    model_name,
+                                    classification_head_name=class_head_name)
+                except Exception as ex:
+                    logger.error(f"During training model {model_name} was caught exception {ex}")
+                    continue
 except KeyboardInterrupt:
     print("The program was interrupted by key signal")
